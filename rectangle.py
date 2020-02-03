2b7531d691f01ce7029b62c2c0e97acf04b26908
Татьяна С. Лобунец <lobunetc_ts@edu.kvantorium-pk.ru>
2020-02-03 11:55:44 +0300

    очень интересно:

---
+++ rectangle.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+def rectangle(l):
+    turtle.forward(l)
+    turtle.left(90)
+    turtle.forward(l)
+    turtle.left(90)
+    turtle.forward(l)
+    turtle.left(90)
+    turtle.forward(l)
+    turtle.left(90)
+
+
+turtle.shape('turtle')
+turtle.forward(50)
+turtle.left(90)
