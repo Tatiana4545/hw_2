a223e92d1f18d49d4c3519cdd2d2d808415c4db2
Татьяна С. Лобунец <lobunetc_ts@edu.kvantorium-pk.ru>
2020-02-15 16:57:50 +1000

    очень интерено

--- 4_nested_rectangle.py
+++ 4_nested_rectangle.py
@@ -1,16 +1,11 @@
 import turtle
 from rectangle import rectangle
 
-def nested_rectangle(n):
-    с = ['red', 'blue', 'green']
-    for i in range(n):
-
-        rectangle(10 + 10+i)
-        turtle.penup()
-        turtle.color(c[i])
-        turtle.goto(-5*i, -5*i)
-        turtle.pendown()
 
 if __name__ == '__main__':
-   for i in range(1, 10, 2):
-       nested_rectangle(5)
+    for i in range(10):
+        rectangle(10+i*10)
+
+
+    while True:
+        pass
